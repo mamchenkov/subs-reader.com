f2cf77e67433da365039cf0630775b9a3d8f212b
Leonid Mamchenkov <leonid@mamchenkov.net>
2013-03-30 14:43:03 +0200

    Added docblocks in global.js

--- app/webroot/js/global.js
+++ app/webroot/js/global.js
@@ -3,10 +3,18 @@ $(document).ready(function() {
 	getFeeds();
 });
 
+/**
+ * Enable keyboard navigation
+ * 
+ * 'j' and 'k' keys move up and down vi-style
+ */
 function enableKeyboard() {
 	$('.media-body').jknavigable();
 }
 
+/**
+ * Fetch the list of user feeds
+ */
 function getFeeds() {
 	$.ajax({
 		url: baseURL + '/users/get_feeds',
